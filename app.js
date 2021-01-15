158477894452c290b3f861eb83ffbc2882c4414c
Paweł Wicher <pwi@epcon.pl>
2021-01-15 19:24:16 +0100

    init app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const lib = require('./src/lib');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send(lib.hello);
+});
+
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('Not Found');
+});
+
+app.listen(port, () => console.log('Ctrl-C to stop'));
