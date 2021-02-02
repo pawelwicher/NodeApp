f263897dbbc76775a3622f0ddbf8488f0d9dc85b
Paweł Wicher <pwi@epcon.pl>
2021-02-02 21:17:07 +0100

    file 2

--- src/file.js
+++ src/file.js
@@ -1,7 +1,9 @@
 exports.getFileContent = getFileContent;
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 
 function getFileContent(filename = path.join(__dirname, '..', 'data/foo.txt')) {
-  return new Promise((resolve, reject) => fs.readFile(filename, (err, data) => err ? reject(err) : resolve(data)));
+  // return new Promise((resolve, reject) => fs.readFile(filename, (err, data) => err ? reject(err) : resolve(data)));
+  return util.promisify(fs.readFile)(filename);
 }
