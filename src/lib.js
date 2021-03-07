dae916b16f99003f8c471dd113fda868a992139e
Pawel Wicher <pawel.wicher@op.pl>
2021-03-07 14:09:19 +0100

    cycle

--- src/lib.js
+++ src/lib.js
@@ -5,6 +5,6 @@ function fib(n) {
   return n < 3 ? 1 : fib(n - 1) + fib(n - 2);
 }
 
-function cycle(arr, times) {
-  return times <= 1 ? arr : [...arr, ...cycle(arr, times - 1)];
+function cycle(arr, times, acc = []) {
+  return times < 1 ? acc : cycle(arr, times - 1, [...acc, ...arr]);
 }
